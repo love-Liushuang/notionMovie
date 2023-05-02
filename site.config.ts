fd62c1c019b2cf61f14ec20404d1e2d787d3c730
liushuangMac <502277775@qq.com>
2023-05-02 21:59:23 +0800

    domain

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '946cc5deae9d4cec9f850b28840de568',
+  rootNotionPageId: '3c66d40dacde43cebb3fcb73696be892',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '1314个人博客名称',
-  domain: 'https://or4444.notion.site/946cc5deae9d4cec9f850b28840de568',
+  domain: 'https://or4444.notion.site/3c66d40dacde43cebb3fcb73696be892?v=c3c34b52c12a4f798ee4b9fb34236caa',
   author: '1314',
 
   // open graph metadata (optional)
